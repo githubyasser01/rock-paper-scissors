4047ed8c995cde03f60d5eb289f7aa9c50f70a26
M. Yasser <m.yasser01@icloud.com>
2022-08-04 19:06:06 +0200

    Write Core Game Algorithm.

--- rps.js
+++ rps.js
@@ -1,3 +1,20 @@
 let getComputerChoice = arr => {
     return arr[Math.floor(Math.random() * arr.length)]
 }
+let arr = ["rock", "paper", "scissors"]
+let computerSelection = getComputerChoice(arr);
+let playerSelection = prompt().toLowerCase();
+if (playerSelection == computerSelection)
+    console.log("Tie!");
+else
+    {switch (playerSelection + computerSelection){
+        case 'ROCKscissors':
+        case 'SCISSORSpaper':
+        case 'PAPERrock':
+            console.log("You win!", (playerSelection.toLowerCase()).charAt(0).toUpperCase() + playerSelection.slice(1), "beats", computerSelection,".");
+            break;
+        default:
+            console.log("You lose!", (playerSelection.toLowerCase()).charAt(0).toUpperCase() + playerSelection.slice(1), "loses against", computerSelection,".");
+            break;
+        }
+    }
